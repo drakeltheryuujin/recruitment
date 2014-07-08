912892b263ce274cb6c2d31ff973fe29ebe81610
drakeltheryuujin <mohawkkellye@hotmail.com>
2014-07-07 20:22:21 -0400

    Fix syntax errors

--- app/controllers/text_over_image_controller.rb
+++ app/controllers/text_over_image_controller.rb
@@ -4,18 +4,18 @@ include Magick
 class TextOverImageController < ApplicationController
   def compile_pic
 
-    img = ImageList.new(‘public/computer-cat.jpg’)
+    img = ImageList.new('public/computer-cat.jpg')
     txt = Draw.new
-    img.annotate(txt, 0,0,0,0, “In ur Railz, annotatin ur picz.”){
+    img.annotate(txt, 0,0,0,0, "In ur Railz, annotatin ur picz."){
     txt.gravity = Magick::SouthGravity
     txt.pointsize = 25
-    txt.stroke = ‘#000000′
-    txt.fill = ‘#ffffff’
+    txt.stroke = '#000000'
+    txt.fill = '#ffffff'
     txt.font_weight = Magick::BoldWeight
     }
 
-    img.format = ‘jpeg’
-    send_data img.to_blob, :stream => ‘false’, :filename => ‘test.jpg’, :type => ‘image/jpeg’, :disposition => ‘inline’
+    img.format = 'jpeg'
+    send_data img.to_blob, :stream => 'false', :filename => 'test.jpg', :type => 'image/jpeg', :disposition => 'inline'
 
   end
 end
