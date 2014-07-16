0d34cc27ce0bdd55a9e1e20dcb6752acae7c22f1
drakeltheryuujin <mohawkkellye@hotmail.com>
2014-07-16 14:07:41 -0400

    remove fog in production and development

--- config/initializers/aws.rb
+++
@@ -1,5 +0,0 @@
-FOG_CONNECTION = Fog::Storage.new({
-  :provider                 => 'AWS',
-  :aws_secret_access_key    => ENV["AWS_SECRET_ACCESS_KEY"],
-  :aws_access_key_id        => ENV["AWS_ACCESS_KEY_ID"]
-})
