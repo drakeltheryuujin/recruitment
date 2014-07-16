03387a93800078a3fbd541219110b1778202141e
drakeltheryuujin <mohawkkellye@hotmail.com>
2014-07-16 15:15:31 -0400

    Add fog path style true to get fog to work in production

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,5 +1,6 @@
 FOG_CONNECTION = Fog::Storage.new({
   :provider                 => 'AWS',
   :aws_secret_access_key    => ENV["AWS_SECRET_ACCESS_KEY"],
-  :aws_access_key_id        => ENV["AWS_ACCESS_KEY_ID"]
+  :aws_access_key_id        => ENV["AWS_ACCESS_KEY_ID"],
+  :path_style               => true
 })
