1ae7699f5337991ca9f49c814a6e8ca65659a221
drakeltheryuujin <mohawkkellye@hotmail.com>
2014-07-16 17:01:23 -0400

    adjusting fog creds

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,5 +1,7 @@
 FOG_CONNECTION = Fog::Storage.new({
   :provider                 => 'AWS',
+  :region                   => 'US-EAST-1',
+  :bucket                   => 'recruitment-app',
   :aws_secret_access_key    => ENV["AWS_SECRET_ACCESS_KEY"],
   :aws_access_key_id        => ENV["AWS_ACCESS_KEY_ID"],
   :path_style               => true
